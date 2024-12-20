01739e2a2ae6d974653784e11500eccb67189b81
Raul Avila Catala <cl2raul66@hotmail.com>
2024-12-20 07:13:03 -0600

    13

--- src/MauiPaletteCreator/MauiPaletteCreator/ViewModels/PgColorsViewModel.cs
+++ src/MauiPaletteCreator/MauiPaletteCreator/ViewModels/PgColorsViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using MauiPaletteCreator.Models;
 using MauiPaletteCreator.Services;
+using MauiPaletteCreator.Tools;
 using MauiPaletteCreator.Views;
 using System.Collections.ObjectModel;
 using System.Reflection;
@@ -11,16 +12,20 @@ namespace MauiPaletteCreator.ViewModels;
 public partial class PgColorsViewModel : ObservableObject
 {
     readonly IColormindApiService colormindApiServ;
+    readonly IStyleTemplateService styleTemplateServ;
 
     public PgColorsViewModel(IStyleTemplateService styleTemplateService, IColormindApiService colormindApiService)
     {
+        styleTemplateServ = styleTemplateService;
         //PrincipalLightColorStyle = new(styleTemplateService.GetDefaultPrincipalLightColorStyle());
         //NeutralLightColorStyle = new(styleTemplateService.GetDefaultNeutralLightColorStyle());
 
         //PrincipalDarkColorStyle = new(styleTemplateService.GetDefaultPrincipalDarkColorStyle());
         LoadCustomPalette();
-        LightColorStyles = new(styleTemplateService.GetDefaultLightColorStyle());
-        DarkColorStyles = new(styleTemplateService.GetDefaultDarkColorStyle());
+        //LightColorStyles = new(styleTemplateService.GetDefaultLightColorStyle());
+        //DarkColorStyles = new(styleTemplateService.GetDefaultDarkColorStyle());
+        LoadLightColorStyle();
+        LoadDarkColorStyle();
         colormindApiServ = colormindApiService;
     }
 
@@ -39,51 +44,51 @@ public partial class PgColorsViewModel : ObservableObject
     [ObservableProperty]
     bool isSelectNEUTRAL;
 
-    [ObservableProperty]
-    ObservableCollection<ColorStyle>? principalLightColorStyle;
+    //[ObservableProperty]
+    //ObservableCollection<ColorStyle>? principalLightColorStyle;
+
+    //[ObservableProperty]
+    //ColorStyle? selectedPrincipalLightColorStyle;
+
+    //[ObservableProperty]
+    //ObservableCollection<ColorStyle>? semanticLightColorStyle;
+
+    //[ObservableProperty]
+    //ColorStyle? selectedSemanticLightColorStyle;
+
+    //[ObservableProperty]
+    //ObservableCollection<ColorStyle>? neutralLightColorStyle;
+
+    //[ObservableProperty]
+    //ColorStyle? selectedNeutralLightColorStyle;
+
+    //[ObservableProperty]
+    //ObservableCollection<ColorStyle>? principalDarkColorStyle;
+
+    //[ObservableProperty]
+    //ColorStyle? selectedPrincipalDarkColorStyle;
+
+    //[ObservableProperty]
+    //ObservableCollection<ColorStyle>? semanticDarkColorStyle;
+
+    //[ObservableProperty]
+    //ColorStyle? selectedSemanticDarkColorStyle;
+
+    //[ObservableProperty]
+    //ObservableCollection<ColorStyle>? neutralDarkColorStyle;
+
+    //[ObservableProperty]
+    //ColorStyle? selectedNeutralDarkColorStyle;
+
 
     [ObservableProperty]
-    ColorStyle? selectedPrincipalLightColorStyle;
-
-    [ObservableProperty]
-    ObservableCollection<ColorStyle>? semanticLightColorStyle;
-
-    [ObservableProperty]
-    ColorStyle? selectedSemanticLightColorStyle;
-
-    [ObservableProperty]
-    ObservableCollection<ColorStyle>? neutralLightColorStyle;
-
-    [ObservableProperty]
-    ColorStyle? selectedNeutralLightColorStyle;
-
-    [ObservableProperty]
-    ObservableCollection<ColorStyle>? principalDarkColorStyle;
-
-    [ObservableProperty]
-    ColorStyle? selectedPrincipalDarkColorStyle;
-
-    [ObservableProperty]
-    ObservableCollection<ColorStyle>? semanticDarkColorStyle;
-
-    [ObservableProperty]
-    ColorStyle? selectedSemanticDarkColorStyle;
-
-    [ObservableProperty]
-    ObservableCollection<ColorStyle>? neutralDarkColorStyle;
-
-    [ObservableProperty]
-    ColorStyle? selectedNeutralDarkColorStyle;
-
-
-    [ObservableProperty]
-    ObservableCollection<ColorStyle>? lightColorStyles;
+    ObservableCollection<ColorStyleGroup>? lightColorStyles;
 
     [ObservableProperty]
     ColorStyle? selectedLightColorStyle;
 
     [ObservableProperty]
-    ObservableCollection<ColorStyle>? darkColorStyles;
+    ObservableCollection<ColorStyleGroup>? darkColorStyles;
 
     [ObservableProperty]
     ColorStyle? selectedDarkColorStyle;
@@ -107,38 +112,38 @@ public partial class PgColorsViewModel : ObservableObject
         else
         {
             var randomColors1 = await colormindApiServ.GetPaletteWithInputAsync([randomColors.First(), null, null, null, randomColors.Last()], "ui");
-            List<ColorStyle> principalLightColor = [];
-            int j = 0;
-            for (int i = 0; i < 5; i++)
-            {
-                if (i == 0)
-                {
-                    var NeutralLightColorStyle0 = NeutralLightColorStyle![0];
-                    NeutralLightColorStyle0.Value = randomColors[0];
-                    NeutralLightColorStyle![0] = NeutralLightColorStyle0;
-                    continue;
-                }
+            //List<ColorStyle> principalLightColor = [];
+            //int j = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    if (i == 0)
+            //    {
+            //        var NeutralLightColorStyle0 = NeutralLightColorStyle![0];
+            //        NeutralLightColorStyle0.Value = randomColors[0];
+            //        NeutralLightColorStyle![0] = NeutralLightColorStyle0;
+            //        continue;
+            //    }
 
-                if (i == 4)
-                {
-                    var NeutralLightColorStyle1 = NeutralLightColorStyle![1];
-                    NeutralLightColorStyle1.Value = randomColors[i];
-                    NeutralLightColorStyle![1] = NeutralLightColorStyle1;
-                    continue;
-                }
+            //    if (i == 4)
+            //    {
+            //        var NeutralLightColorStyle1 = NeutralLightColorStyle![1];
+            //        NeutralLightColorStyle1.Value = randomColors[i];
+            //        NeutralLightColorStyle![1] = NeutralLightColorStyle1;
+            //        continue;
+            //    }
 
-                if (i > 0 && i < 4)
-                {
-                    var NeutralLightColorStyle1_3 = NeutralLightColorStyle![i];
-                    NeutralLightColorStyle1_3.Value = randomColors1[i];
-                    NeutralLightColorStyle![i] = NeutralLightColorStyle1_3;
-                }
+            //    if (i > 0 && i < 4)
+            //    {
+            //        var NeutralLightColorStyle1_3 = NeutralLightColorStyle![i];
+            //        NeutralLightColorStyle1_3.Value = randomColors1[i];
+            //        NeutralLightColorStyle![i] = NeutralLightColorStyle1_3;
+            //    }
 
-                principalLightColor.Add(PrincipalLightColorStyle![j]);
-                principalLightColor[j].Value = randomColors[i];
-                j++;
-            }
-            PrincipalLightColorStyle = [.. principalLightColor];
+            //    principalLightColor.Add(PrincipalLightColorStyle![j]);
+            //    principalLightColor[j].Value = randomColors[i];
+            //    j++;
+            //}
+            //PrincipalLightColorStyle = [.. principalLightColor];
         }
     }
 
@@ -155,9 +160,16 @@ public partial class PgColorsViewModel : ObservableObject
     }
 
     #region EXTRA
-    partial void OnSelectedPrincipalLightColorStyleChanged(ColorStyle? value)
+    partial void OnIsSelectDarkThemeChanged(bool value)
     {
-
+        if (value)
+        {
+            SelectedLightColorStyle = null;
+        }
+        else
+        {
+            SelectedDarkColorStyle = null;
+        }
     }
 
     void LoadCustomPalette()
@@ -176,5 +188,35 @@ public partial class PgColorsViewModel : ObservableObject
 
         DefaultPalette = [.. allColors];
     }
+
+    void LoadLightColorStyle()
+    {
+        var defaultTemplate = styleTemplateServ.GetDefaultLightColorStyle();
+        if (defaultTemplate is not null)
+        {
+            List<ColorStyleGroup> groups = [];
+            foreach (var item in defaultTemplate!.Where(x => x.Scheme == ColorScheme.Light).GroupBy(cs => cs.Tag))
+            {
+                var s = item.ToArray();
+                var group = new ColorStyleGroup(item.Key!, item.ToArray());
+                groups.Add(group);
+            }
+            LightColorStyles = [.. groups];
+        }
+    }
+
+    void LoadDarkColorStyle()
+    {
+        var defaultTemplate = styleTemplateServ.GetDefaultDarkColorStyle();
+        List<ColorStyleGroup> groups = [];
+        foreach (var item in defaultTemplate!.Where(x => x.Scheme == ColorScheme.Dark).GroupBy(cs => cs.Tag))
+        {
+            var s = item.ToArray();
+            var group = new ColorStyleGroup(item.Key!, item.ToArray());
+            groups.Add(group);
+        }
+        DarkColorStyles = [.. groups];
+    }
+
     #endregion
 }
